72da4e8e84df91bca5a4bbe305ed7e688cf47389
HYEONHUI <hh.blaire.cho@gmail.com>
2022-10-02 02:06:45 +0900

    update AppTest.java
    .add more tests (equalThat etc)
    .add mockito dependency
    .so add mockito test

---
+++ src/main/java/org/example/Complicate.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Complicate {
+
+    String func() {
+        return "abcde";
+    }
+}
+
+
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,43 @@
 package org.example;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    String apple;
+    String banana;
+
+    @Before
+    public void setup() {
+        apple = "APPLE";
+        banana = "BANANA";
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+        assertEquals(apple, "APPLE");
+        assertThat(banana, equalTo("BANANA"));
+    }
+
+    @Test
+    public void testMockito() {
+        Complicate cp = mock(Complicate.class);
+        when(cp.func()).thenReturn("qwerty");
+        assertThat(cp.func(), equalTo("qwerty"));
+    }
+
+    @Test
+    public void testNonMockito() {
+        Complicate cp = new Complicate();
+        assertThat(cp.func(), equalTo("abcde"));
     }
 }
