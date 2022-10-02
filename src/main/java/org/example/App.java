2147e27477af705ba0e9fa29be5384a9067e00d0
HYEONHUI <hh.blaire.cho@gmail.com>
2022-10-03 00:02:17 +0900

    start to use its submodule class CommonApp
    so pom file updated

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,5 +4,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        CommonApp common = new CommonApp();
+        String s = common.createGreeting("James");
+        System.out.print(s);
     }
 }
